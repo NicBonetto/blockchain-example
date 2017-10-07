f5525417fde1d4fcbfb464a18ff96b75087262c8
Nic Bonetto <nick.bonetto@gmail.com>
2017-10-07 15:45:13 -0700

    Defined new_block method. #3

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,23 @@
 class Blockchain(object):
     def __init__(self):
+
         self.chain = []
         self.current_transactions = []
+
+    def new_block(self, proof, previous_hash=None):
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block
     
     def new_transaction(self, sender, recipient, amount):
 
