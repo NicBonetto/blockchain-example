451823fb7bf3a01a0b3d41d94cacb47e2c24899b
Nic Bonetto <nick.bonetto@gmail.com>
2017-10-07 15:31:38 -0700

    Defined new_transaction method. #2

--- blockchain.py
+++ blockchain.py
@@ -2,3 +2,13 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+    
+    def new_transaction(self, sender, recipient, amount):
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
